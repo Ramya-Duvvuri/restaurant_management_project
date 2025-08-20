06b26bab4fc4c6cb5a57e92e342709abe4463afe
Ramya <120212923+username-04@users.noreply.github.com>
2025-08-20 17:41:54 +0530

    32915

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.db import DatabaseError
+
+def signup_view(request):
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        try:
+            if form.is_valid():
+                form.save()
+                messages.success(request,'Account created successfully.Please log in')
+                return redirect('login')
+            else:
+                messages.error(request,'Please correct the errors below')
+        except DatabaseError as db_error:
+            messages.error(request,f"Database error has been occurred: {db_error}")
+        except Exception as e:
+            messages.error(request,f"An unexpected error has been occured:{e}")
+    else:
+        form = UserCreationForm()
+    return render(request,'signup.html',{'form':form})
+
 
 # Create your views here.
