176ee68a65653b316ccc47351440c64cdcba2326
Ramya <120212923+username-04@users.noreply.github.com>
2025-09-02 12:53:37 +0530

    37983

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+def home(request)::
+    api_url  = "http://127.0.0.1:8000/api/menu/"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        menu_items =  response.json()
+    else:
+        menu_items = []
+    return render(request,"home.html",{"menu_items":menu_itemss0})
+
