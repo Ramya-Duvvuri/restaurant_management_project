0c6f91806f1ff71620909731a7b73f161b55f6fd
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-09-13 18:47:47 +0530

    42779

--- products/models.py
+++ products/models.py
@@ -8,3 +8,10 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+class MenuItems(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 500)
+    price = models.DecimalField(max_digits = 10,decimal_places = 2)
+
+    def __str__(self):
+        return str(self.name)
