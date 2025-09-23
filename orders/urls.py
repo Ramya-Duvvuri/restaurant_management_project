2cba2c3e68acc0bee7ede1b889966102604a3eba
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-09-23 22:11:40 +0530

    44578

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
-
+from django.config,urls.static import static
 urlpatterns = [
     
 ]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
