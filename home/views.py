5c47cff0ddaceb38e804849694dbe1971f1bf1b4
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-09-27 21:28:39 +0530

    51476

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.config import settings
+from .serializers import MenuCategorySerializer
+
+class MenuCategoryListView(ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
+
+
 # Create your views here.
 def home_view(request):
     restaurant_name = getattr(settings,'RESTAURANT_NAME','My Restaurant')
@@ -11,4 +18,13 @@ def home_view(request):
 def reservations_view(request):
     return render(request,'reservations.html')
 def contact_view(request):
-    return render(request,'contact.html')
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return.redirect("Thank_you")
+    else:
+        form = ContactForm()
+    return render(request,"contact_form.html",{"form":form})
+
+
