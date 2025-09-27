1cfe2ed4de01a197edb6720baf429a26a084cb39
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-09-27 21:29:55 +0530

    51476

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('reservations/'views.reservations_view , name ='reservations'),
+    path('reservations/',views.reservations_view , name ='reservations'),
+    path('contact/',contact_view,name ="contact"),
+    path('thank_you/',lambda request:render(request,"thankyou.html"),name ="thankyou")
+    path('categories/',MenuCategoryListView.as_view(),name = 'menu-categories')
 ]
