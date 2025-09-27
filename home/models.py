4742f440651dad2cdf4e92a95f273a428d646be1
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-09-27 21:12:06 +0530

    51472

--- home/models.py
+++ home/models.py
@@ -12,3 +12,8 @@ class Restaurant(models.Model):
     
     def __str__(self):
         return self.name
+class MenuCategory(models.Model):
+    name = models.CharField(max_length = 50, unique = True)
+
+    def __str__(self):
+        return self.name
