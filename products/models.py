5a1a09f5968ecc48b7821e36d8812bc8e2889a6f
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-10-01 23:17:14 +0530

    52049

--- products/models.py
+++ products/models.py
@@ -13,6 +13,7 @@ class MenuItems(models.Model):
     description = models.CharField(max_length = 500)
     price = models.DecimalField(max_digits = 10,decimal_places = 2)
     image = models.ImageField(upload_to ='menu_images/',blank =  True,null= True)
+    available = models.BooleanField(default = True)
 
     def __str__(self):
         return str(self.name)
