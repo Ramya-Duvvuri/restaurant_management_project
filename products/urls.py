d11194bede1bf008822f29004536cd1919fbea4d
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-10-01 23:20:56 +0530

    52049

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/',MenuListView.as_view(),name ='menu-list'),
+    router.register(r'menu=items',MenuListView,base_name = 'menuitem')
+    urlpatterns = router.urls
 ]
