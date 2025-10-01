a7803b72706a42b86113ce26d05801c4875e91e7
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-10-01 23:18:59 +0530

    52049

--- products/serializers.py
+++ products/serializers.py
@@ -10,3 +10,8 @@ class MenuItemsSeriaizer(serializer.ModelSerializer):
     class Meta:
         model = MenuItems
         fields = ['id','name','description','price']
+
+        def validate_price(self,value):
+            if value <0 :
+                raise serializers.validationError('Price must be a positive value')
+                return value
