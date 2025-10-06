31cf3df404b1de5ff1c2d4e686ec9d4be3fa559f
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-10-06 17:09:52 +0530

    53244

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 from django.config,urls.static import static
 urlpatterns = [
-    
+    path('coupons/validate/',Couponvalidationview.as_view(),name = 'coupon-validate'),
 ]
 if settings.DEBUG:
     urlpatterns +=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
