22f495ff4929f608e92611961b9cccf4f148ce78
Duvvuri Ramya Bhavajna <120212923+Ramya-Duvvuri@users.noreply.github.com>
2025-10-08 23:10:51 +0530

    53242

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('contact/',contact_view,name ="contact"),
     path('thank_you/',lambda request:render(request,"thankyou.html"),name ="thankyou")
     path('categories/',MenuCategoryListView.as_view(),name = 'menu-categories')
+    path('api/tables/',TableListView.as_view(),name = 'table-list')
+    path('api/tables/<int:pk>',TableListView.as_view(),name = 'table-detail')
 ]
